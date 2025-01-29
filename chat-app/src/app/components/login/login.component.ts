7bb0033c8332ebf50db33828a0e5bb2029778a9d
MarcelDieter <marcel.dieter@h-net.com>
2025-01-29 14:09:29 +0100

    adding form control

--- chat-app/src/app/components/login/login.component.ts
+++ chat-app/src/app/components/login/login.component.ts
@@ -1,20 +1,33 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { CreateAccountComponent } from '../create-account/create-account.component';
+import { MatIcon } from '@angular/material/icon';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [MatDialogModule, MatButtonModule, MatFormFieldModule, MatInput],
+  imports: [MatDialogModule, MatButtonModule, MatFormFieldModule, MatInput, MatIcon, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm?: FormGroup;
+
   private dialog = inject(MatDialog);
   private dialogRef = inject(MatDialogRef<LoginComponent>);
+  private fb = inject(FormBuilder);
   
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['',Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
   login() {}
   openCreateAccount() {
     this.dialogRef.close();
